d8527404078f0be5b2a3b142a71f16f528990894
GodVov4 <volume245@gmail.com>
2023-11-22 16:27:30 +0200

    create second queue

--- producer.py
+++ producer.py
@@ -1,8 +1,10 @@
 from configparser import ConfigParser
 from faker import Faker
-from mongoengine import connect, Document, StringField, EmailField, BooleanField
+from json import dumps
+from mongoengine import connect, Document, StringField, EmailField, BooleanField, IntField
 from pathlib import Path
 from pika import PlainCredentials, BlockingConnection, ConnectionParameters, BasicProperties, spec
+from random import choice
 
 
 fake = Faker('uk_UA')
@@ -10,14 +12,17 @@ fake = Faker('uk_UA')
 
 class Contact(Document):
     fullname = StringField(max_length=120)
-    email = EmailField()
+    phone = IntField()
+    email = EmailField(max_length=120)
+    priority = StringField(max_length=5, default='email')
     ack = BooleanField(default=False)
     meta = {'collection': 'contacts'}
 
 
 def create_contact(count: int = 1) -> None:
     for _ in range(count):
-        Contact(fullname=fake.name(), email=fake.ascii_free_email()).save()
+        Contact(fullname=fake.name(), phone=fake.msisdn(), email=fake.ascii_free_email(),
+                priority=choice(('phone', 'email'))).save()
 
 
 def send_message(count: int = 1) -> None:
@@ -26,20 +31,30 @@ def send_message(count: int = 1) -> None:
     channel = connection.channel()
 
     channel.exchange_declare(exchange='task_mock', exchange_type='direct')
-    channel.queue_declare(queue='task_queue', durable=True)
-    channel.queue_bind(exchange='task_mock', queue='task_queue')
+    channel.queue_declare(queue='sms_queue', durable=True)
+    channel.queue_bind(exchange='task_mock', queue='sms_queue')
+    channel.queue_declare(queue='email_queue', durable=True)
+    channel.queue_bind(exchange='task_mock', queue='email_queue')
 
     contacts = Contact.objects.all()
     for contact in contacts:
-        message = f'ID: {contact.id}, name: {contact.fullname}'
+        message = {'ID': str(contact.id), 'name': contact.fullname}
 
-        channel.basic_publish(
-            exchange='task_mock',
-            routing_key='task_queue',
-            body=message.encode(),
-            properties=BasicProperties(
-                delivery_mode=spec.PERSISTENT_DELIVERY_MODE
-            ))
+        match contact.priority:
+            case 'email':
+                channel.basic_publish(
+                    exchange='task_mock',
+                    routing_key='email_queue',
+                    body=dumps(message).encode(),
+                    properties=BasicProperties(delivery_mode=spec.PERSISTENT_DELIVERY_MODE)
+                )
+            case 'phone':
+                channel.basic_publish(
+                    exchange='task_mock',
+                    routing_key='sms_queue',
+                    body=dumps(message).encode(),
+                    properties=BasicProperties(delivery_mode=spec.PERSISTENT_DELIVERY_MODE)
+                )
 
         print(f' [x] Sent {message}')
     connection.close()
